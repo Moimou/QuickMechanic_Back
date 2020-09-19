2d8a330c269207aae4bff684b9be92dacfa33b5c
DJOMO SANDRA <sandradjomo@gmail.com>
2020-09-19 17:16:01 +0100

    trying to fix mongodb

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { buildSchema} = require('graphql');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+let port = process.env.PORT || 4000;
+
 
 const BreakDown = require('./models/breakdown');
 const Driver = require('./models/driver');
@@ -170,7 +172,9 @@ app.use('/graphql',
 // mongodb+srv://${process.env.MONGO_USER}:${process.env.MONG0_PASSWORD}@cluster0.81el4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority
 mongoose.connect(`mongodb+srv://Sandra:L5ZnZ4LfjAvzHWSV@cluster0.81el4.mongodb.net/mechanic-react-dev?retryWrites=true&w=majority`)
     .then(() => {
-        app.listen(4000);
+        app.listen(port, () =>{
+            console.log(`Exemple app is listening on port http://localhost:${port}`);
+        });
     })
     .catch((error) => { console.log(error); })
 
