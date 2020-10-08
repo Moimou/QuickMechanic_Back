5a996fb31a0f33f9dd53eb00771774a581d6f7ba
DJOMO SANDRA <sandradjomo@gmail.com>
2020-10-08 15:19:45 +0100

    create new breakdown3

--- graphql/resolvers/breakdowns.js
+++ graphql/resolvers/breakdowns.js
@@ -2,7 +2,7 @@ const BreakDown = require('../../models/breakdown');
 const Driver = require('../../models/driver');
 const Mechanic = require('../../models/mechanic');
 
-const {transformBreakdown} = require('./merge');
+const { transformBreakdown } = require('./merge');
 
 
 
@@ -12,7 +12,7 @@ module.exports = {
             const breakdowns = await BreakDown.find();
             return breakdowns.map(breakdown => {
                 return transformBreakdown(breakdown);
-                
+
 
             });
         } catch (err) {
@@ -20,7 +20,7 @@ module.exports = {
         }
     },
     createBreakDown: async (args, req) => {
-        if(!req.isAuth){
+        if (!req.isAuth) {
             throw new Error('Unauthenticated');
         }
         const breakdown = new BreakDown({
@@ -30,37 +30,37 @@ module.exports = {
             location_lon: args.breakdownInput.location_lon,
             location_lat: args.breakdownInput.location_lat,
             license_plate: args.breakdownInput.license_plate,
-            creator: req.userId
+            creator: '5f763369c6af2b00175c3d24'
         })
         let createdBreakdown;
-        
-        try{
-        const result = await breakdown.save();                        
-                createdBreakdown = transformBreakdown(result);        
-            
+
+        try {
+            const result = await breakdown.save();
+            createdBreakdown = transformBreakdown(result);
+
             const listOfAllMechanic = await Mechanic.find();
-            const creator = await Driver.findById(req.userId);
+            const creator = await Driver.findById('5f763369c6af2b00175c3d24');
 
-            
 
-                if (!creator) {
-                    throw new Error("driver not found");
-                }
-                creator.createdBreakdowns.push(breakdown);
-                await creator.save();
-                
-                return{createdBreakdown, listOfAllMechanic};
 
-                //return createdBreakdown ;
-           } catch (err){ 
-                console.log(err);
-                throw err;
+            if (!creator) {
+                throw new Error("driver not found");
             }
+            creator.createdBreakdowns.push(breakdown);
+            await creator.save();
+
+            //return{createdBreakdown, listOfAllMechanic};
+
+            return createdBreakdown;
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
     },
 
-    
 
-    
+
+
 };
 
 
--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -23,6 +23,6 @@ module.exports = (req, res, next) => {
         return next();
     }
     req.isAuth = true;
-    req.userId = jwt.decodedToken.userId;
+    req.userId = decodedToken.userId;
     next();
 }
